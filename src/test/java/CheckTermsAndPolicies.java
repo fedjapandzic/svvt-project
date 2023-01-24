49c869c2b8c4e3cc898f2e76727cbe55338b08f0
Fedja Pandzic <fedjapandzic1@gmail.com>
2023-01-24 23:37:06 +0100

    quickfix

--- src/test/java/CheckTermsAndPolicies.java
+++ src/test/java/CheckTermsAndPolicies.java
@@ -5,8 +5,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
@@ -21,6 +23,7 @@ public class CheckTermsAndPolicies {
     static void setUp() throws Exception{
         driver = new ChromeDriver();
         baseUrl = "https://www.comtrade.com/";
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
     }
     @AfterAll
     static void close() throws Exception{
@@ -37,6 +40,8 @@ public class CheckTermsAndPolicies {
         assertEquals("https://www.comtrade.com/terms-of-use/",driver.getCurrentUrl());
         driver.navigate().back();
         Thread.sleep(1000);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[3]/span")));
+        driver.findElement(By.xpath("/html/body/div[3]/span")).click();
         WebElement privacyPolicy = driver.findElement(By.xpath("/html/body/footer/div[1]/div/ul/li[3]/a"));
         privacyPolicy.click();
         Thread.sleep(1000);
